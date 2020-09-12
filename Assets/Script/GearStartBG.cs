512b794632d64b5578c58024831de50b6ff1a8d8
YanTree <yantreeland@outlook.com>
2020-09-12 16:42:18 +0800

    rotate gear in menu

---
+++ Assets/Script/GearStartBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearStartBG : MonoBehaviour
+{
+    public float rotationSpeed = 40f;
+    public float direction = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+            StartRotate();
+
+    }
+
+    void StartRotate() // 齿轮旋转函数
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * direction * Time.deltaTime, Space.Self);
+    }
+}
